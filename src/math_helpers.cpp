baf4e25973feabd4b3b3f2235acd74a47008f502
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-01-14 21:24:12 +0100

    noise in corners

--- src/math_helpers.cpp
+++ src/math_helpers.cpp
@@ -32,7 +32,7 @@ coordinates massCenter(image* frame, config* cfg){
 	uint32_t sumX = 0, sumY = 0, sumTotal = 0, x, y, pixel;
 	coordinates move;
 
-	for (y = 0; y < cfg->imageResY; y += cfg->centerSkipPixels){ // loop y axis
+	for (uint32_t y = 0; y < cfg->imageResY; y += cfg->centerSkipPixels){ // loop y axis
 		for (x = 0; x < cfg->imageResX; x += cfg->centerSkipPixels){ // loop x axis
 			pixel = frame->rawBitmap[x + y*  cfg->imageResX];
 			if (pixel >= cfg->centerThreshold){
@@ -61,12 +61,43 @@ void calcNoiseCorners(image *imgData, config* cfg){
 	}
 	triHeight = sqrt(2*pow(maxDiameter,2));
 	triLeg = (uint32_t)sqrt(2)*triHeight;
-	numPixels = (uint32_t)0.5 * pow(2*triLeg,2);
-	
+	numPixels = (uint32_t)(0.5 * pow(2*triLeg,2));
+	uint32_t pixels[numPixels];
+	uint32_t y,counter;
+/*
+ *   0:0   0:1   0:2    0:3 …   0:97   0:98   0:99   0:100
+ *   1:0   1:1   1:2        …          1:98   1:99   1:100
+ *   2:0   2:1              …                 2:99   2:100
+ *   3:0                    …                        3:100
+ *    …     …     …      …  …    …      …      …      …
+ *  97:0                    …                        7:100
+ *  98:0  98:1              …                98:99  98:100
+ *  99:0  99:1  99:2        …	      99:98  99:99  99:100
+ * 100:0 100:1 100:2  100:3 … 100:97 100:98 100:99 100:100
+ */
+	// upper left corner
 	// take pixels in triangle beginning in corner and move in one pixel at a time
-	for (n = 0; y <= triLeg; n++){
-		
+	for (uint32_t n = 0; n <= triLeg; n++){
+		for (uint32_t x = 0; x < triLeg; x++){
+			// sum of x and y coords equals n, split between x and y
+			y = n - x;
+			pixels[counter] = imgData->rawBitmap[x][y];
+			counter++;
+		}
 	}
+	upperLeft = calcNoise(pixels);
+	
+	counter = 0;
+	// bottom left
+	for (uint32_t n = cfg->imageResY; n <= triLeg; n--){
+		for (uint32_t y = 0; y < triLeg; y++) {
+			// sum of x and y coords equals n, split between x and y
+			x = n - y;
+			pixels[counter] = imgData->rawBitmap[x][y];
+			counter++;
+		}
+	}
+	bottomLeft = calcNoise(pixels);
 }	
 
 noise calcNoise(uint32_t *pixels){
