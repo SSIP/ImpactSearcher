a664208fb8a038bb7ebe523b03d1a8437a015284
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-01-15 00:43:25 +0100

    fix corner calcs

--- src/math_helpers.cpp
+++ src/math_helpers.cpp
@@ -1,5 +1,6 @@
 #include "math_helpers.h"
 #include "definitions.h"
+#include <algorithm>
 
 coordinates rayCenter(coordinates approximateCenter, image* frame, int32_t numberRays, config* cfg){
 	int32_t i;
@@ -51,7 +52,7 @@ coordinates massCenter(image* frame, config* cfg){
 
 void calcNoiseCorners(image *imgData, config* cfg){
 	uint32_t maxDiameter, triHeight, triLeg, numPixels;
-	noise topLeft, topRight, bottomLeft, bottomRight;
+	noise corners[4];
 	
 	if (cfg->imageResX > cfg->imageResY){
 		maxDiameter = (uint32_t)(cfg->imageResY + cfg->imageResY * 0.2);
@@ -62,12 +63,13 @@ void calcNoiseCorners(image *imgData, config* cfg){
 	triHeight = sqrt(2*pow(maxDiameter,2));
 	triLeg = (uint32_t)sqrt(2)*triHeight;
 	numPixels = (uint32_t)(0.5 * pow(2*triLeg,2));
-	uint32_t pixels[numPixels];
+	uint32_t *pixels = new uint32_t[numPixels];
 	uint32_t y,counter;
 
 /*
  * image[x][y]
  *  0 -------------------------------------------------------> X axis
+ *  |  Quadrant 2                                  Quadrant 1
  *  |  top left                                     top right
  *  |    0:0   0:1   0:2   0:3 …   0:97   0:98   0:99   0:100
  *  |    1:0   1:1   1:2       …          1:98   1:99   1:100
@@ -79,6 +81,7 @@ void calcNoiseCorners(image *imgData, config* cfg){
  *  |   99:0  99:1  99:2       …	     99:98  99:99  99:100
  *  |  100:0 100:1 100:2 100:3 … 100:97 100:98 100:99 100:100
  *  |  bottom left                               bottom right
+ *  |  Quadrant 3                                  Quadrant 4
  *  v
  * 
  *  Y axis
@@ -87,7 +90,7 @@ void calcNoiseCorners(image *imgData, config* cfg){
 	// top left
 	for (uint32_t x = 0; x < triLeg; x++){
 		for (uint32_t y = 0; y <= triLeg - x; y++){
-			pixels[counter] = imgData->rawBitmap[x][y];
+			pixels[counter] = imgData->rawBitmap[y][x];
 			counter++;
 		}
 	}
@@ -97,7 +100,7 @@ void calcNoiseCorners(image *imgData, config* cfg){
 	// bottom left
 	for (uint32_t x = 0; x < triLeg; x++){
 		for (uint32_t y = cfg-imageResY - triLeg + x; y < cfg->imageResY ; y++){
-			pixels[counter] = imgData->rawBitmap[x][y];
+			pixels[counter] = imgData->rawBitmap[y][x];
 			counter++;
 		}
 	}
@@ -107,7 +110,7 @@ void calcNoiseCorners(image *imgData, config* cfg){
 	// top right
 	for (uint32_t x = cfg->imageResX - triLeg; x < cfg->imageResX; x++){
 		for (uint32_t y = 0; y < (x - cfg->imageResX + triLeg); y++){
-			pixels[counter] = imgData->rawBitmap[x][y];
+			pixels[counter] = imgData->rawBitmap[y][x];
 			counter++;
 		}
 	}
@@ -117,13 +120,46 @@ void calcNoiseCorners(image *imgData, config* cfg){
 	// bottom right
 	for (uint32_t x = cfg->imageResX - triLeg; x < cfg->imageResX; x++){
 		for (uint32_t y = cfg->imageResY - (x - cfg->imageResX + trileg); y < cfg->imageResY; y++){
-			pixels[counter] = imgData->rawBitmap[x][y];
+			pixels[counter] = imgData->rawBitmap[y][x];
 			counter++;
 		}
 	}
 	bottomRight = calcNoise(pixels);
+	
+	// least squares differences in avg and std dev, use the 3 closest corners
+	int myints[] = {1,2,3,4};
+
+	std::sort (myints,myints+4);
+
+	do {
+		// calculate "sum" of averages and variances
+	} while ( std::next_permutation(myints,myints+4) );
+
+	delete[] pixels;
 }	
 
 noise calcNoise(uint32_t *pixels){
-	
+
+}
+
+double getMean()
+{
+	double sum = 0.0;
+	for(double a : data)
+		sum += a;
+	return sum/size;
+}
+
+double getVariance()
+{
+	double mean = getMean();
+	double temp = 0;
+	for(double a :data)
+		temp += (a-mean)*(a-mean);
+	return temp/size;
+}
+
+double getStdDev()
+{
+	return Math.sqrt(getVariance());
 }
