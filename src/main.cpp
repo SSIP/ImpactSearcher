7dd175727f92e28440f6ab0a96bddb47443da02f
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-12-16 14:50:36 +0100

    Function that provides reasonable default values

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,25 @@ LIB_PUBLIC void impactSearcherStop(config *cfg) {
 	cfg->shutdownThread = 1;
 }
 
+/* Set a standard configuration
+ *
+ * Param *cfg is the reference to the configuration struct
+ */
+void loadDefaultConfig(config* cfg) {
+	// set resolution to 0 if unknown
+	cfg->imageResX = 0;
+	cfg->imageResY = 0;
+	cfg->keepFrames = KEEP_ALL;
+	cfg->centerSkipPixels = 5;
+	cfg->centerAlgo = CENTER_OF_MASS;
+	cfg->centerThreshold = 50;
+	cfg->averageLength = 10;
+	cfg->checkSNR = 5.0;
+	cfg->checkRadius = 1.0;
+	cfg->verbosity = 1;
+	cfg->rayBrightnessThreshold = 0.3;
+	cfg->maxDiameter = 0.8;
+	cfg->leadingAverageLength = 5;
+	cfg->trailingAverageLength = 20;
+	cfg->framebufferLength = 20;
+}
